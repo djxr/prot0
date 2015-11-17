f167c52f92c4db25e96f855056424d35996d9cac
Antony Root <antoneh@gmail.com>
2015-11-17 17:35:31 -0500

    Window boilerplate

--- main.c
+++ main.c
@@ -3,49 +3,73 @@
 #include <SDL2/SDL.h>
 
 // STATE ENUMS /*{{{*/
-enum game_state {
+enum game_state
+{
 	cutscene,
 	menu,
 	overworld,
 	underworld
 };
-enum player_state {
+enum player_state
+{
 	stand,
 	mov_up,
 	mov_left,
 	mov_right,
 	mov_down
 };
-enum ai_state {
+enum ai_state
+{
 	offscreen,
 	idle
 };/*}}}*/
 // STATE MACHINE STRUCTURES /*{{{*/
-struct game_t {
+struct game_t
+{
 	enum game_state state;
 };
-struct player_t {
+struct player_t
+{
 	enum player_state state;
 };
-struct ai_t {
+struct ai_t
+{
 	enum ai_state state;
 };/*}}}*/
 
-init(){
-	if(SDL_Init(SDL_INIT_VIDEO)){
-		fprintf("SDL Init failed: %s\n",SDL_GetError());
+SDL_Window	*win;
+
+int init() /*{{{*/
+{
+	if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS))
+	{
+		fprintf(stderr, "SDL Init failed: %s\n",SDL_GetError());
 	}
+
+	win = SDL_CreateWindow(
+			"windows yo.",
+			0, 0,
+			640, 480,
+			0);
+
 	SDL_Log("I initialized");
-}
-
-tini(){
+	return 0;
+}/*}}}*/
+int tini() /*{{{*/
+{
+	SDL_DestroyWindow(win);
 	SDL_Quit();
-	SDL_Log("I de-initialized");
-}
 
-int main(int argc, char *argv[]){
+	SDL_Log("I de-initialized");
+	return 0;
+}/*}}}*/
+
+int main(int argc, char *argv[])
+{
 
 	init();
+
+	SDL_Delay(2000);
 
 	tini();
 	return 0;
